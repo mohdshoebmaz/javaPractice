0f2bde31c68335c2f90f45bd48e1358489662622
mohdshoebmaz <Saiba@1122>
2021-12-23 00:09:07 +0530

    First git project seond day

---
+++ src/main/java/app1.java
@@ -0,0 +1,9 @@
+public class app1 {
+    public static void main( String[] args )
+    {
+        int String = 111;
+        int Runnable = 999;
+        System.out.println( String );
+        System.out.println( Runnable );
+    }
+}
