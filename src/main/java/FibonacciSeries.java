0920a7fa3f4afc54f3b4d3eaaefb39dbdf564fa7
mohdshoebmaz <Saiba@1122>
2021-12-23 01:10:53 +0530

    fabonacci series program

---
+++ src/main/java/FibonacciSeries.java
@@ -0,0 +1,13 @@
+class FibonacciSeries {
+    public static void main(String[] args){
+        long n1 = 0;
+        long n2 = 1;
+        System.out.print(n1 + ", " + n2);
+        for(int i=2;i<=20;i++){
+            long n = n1 + n2;
+            System.out.print(", " + n);
+            n1 = n2;
+            n2 = n;
+        }
+    }
+}
--- src/main/java/app.java
+++ src/main/java/app.java
@@ -2,6 +2,11 @@ class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        int x = 0;
+        if(x==(1-2)){
+            System.out.println("if");
+        }else{
+            System.out.println("else");
+        }
     }
 }
